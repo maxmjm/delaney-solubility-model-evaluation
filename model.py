1ae548a8e599f9b605dbbf82be285f5bc6e35026
Maxime Morard <maxmjm@icloud.com>
2025-01-20 17:47:17 +0800

    Separate data as x and y

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
 import pandas 
 
 dataframe = pandas.read_csv("delaney_solubility_with_descriptors.csv")
+
+# Separate data as y
+y = dataframe["logS"]
+print(y)
+
+# Separate data as x
+x = dataframe.drop("logS", axis=1)
+print(x)
