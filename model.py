4ce4909befff5c6864ee69a5338c33a6e276bc2c
Maxime Morard <maxmjm@icloud.com>
2025-01-20 17:55:17 +0800

    Split data into training set 80% and test set 20%

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas 
+from sklearn.model_selection import train_test_split
 
 dataframe = pandas.read_csv("delaney_solubility_with_descriptors.csv")
 
@@ -9,3 +10,8 @@ print(y)
 # Separate data as x
 x = dataframe.drop("logS", axis=1)
 print(x)
+
+# Split data into training set 80% and test set 20%
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
+
+print(x_test)
